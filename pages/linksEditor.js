bef6eba1ae960f1ce91035d4592e42391d330a8d
Juan Gerardo Franco <juangerardofranco@gmail.com>
2023-07-08 13:42:56 -0500

    styling of adding and editing links

--- pages/linksEditor.js
+++ pages/linksEditor.js
@@ -5,7 +5,7 @@ import { getSession, useSession, signOut } from "next-auth/react"
 import connectMongo from '../database/conn';
 import Links from '../model/LinkSchema';
 import Layout from '../layouts/pagelayout';
-import { HiOutlinePencil, HiOutlinePlusSm }  from 'react-icons/hi'
+import { HiOutlinePencilAlt, HiOutlinePlusSm, HiOutlineXCircle, HiOutlineCloudUpload}  from 'react-icons/hi'
 import { useState } from 'react';
 import { set } from 'mongoose';
 
@@ -38,153 +38,133 @@ function User({ session, handleSignOut, links }){
       return b.index - a.index;
   });
 
+  // edit link handlers
   function handleEditLink(e, link){
     e.preventDefault();
-    //console.log("I've been clicked", link._id);
     setCurrentLink(link)
     setShowEditLink(true);
   }
-  function handleCancelEditLink(e){
+  function handleCancel(e){
     e.preventDefault();
-    setShowEditLink(false);
+    if (showEditLink) setShowEditLink(false);
+    if (showAddLink) setShowAddLink(false)
+    
   }
-
 
   // add link handlers
   function handleAddLink(e){
     e.preventDefault();
+    setCurrentLink({...currentLink, active: 'true', url:'', title:'', index: links.length})
     setShowAddLink(true)
   }
-
-  function handleCancelAddLink(e){
-    e.preventDefault();
-    setShowAddLink(false)
-  }   
-
-  if(showEditLink){
-    return(
-      <section className="container mx-auto text-center py-20 w-4/5 sm:w-1/2">
-        <div className='flex flex-col items-center'>
-          <form>
-            <input 
-              type="text"
-              name='title'
-              placeholder='title'
-              value={currentLink.title}
-              className={styles.input_text}
-              onChange={(e)=>setCurrentLink({...currentLink, title : e.target.value})}
-              /* {...formik.getFieldProps('email')} */
-            />
-            <input 
-              type="text"
-              name='url'
-              placeholder='url'
-              value={currentLink.url}
-              className={styles.input_text}
-              onChange={(e)=>setCurrentLink({...currentLink, url : e.target.value})}
-              /* {...formik.getFieldProps('email')} */
-            />
-            <p>active:
-            <input 
-              type="checkbox"
-              name='active'
-              placeholder='active'
-              className={styles.input_text}
-              checked={currentLink.active === 'true'}
-              onChange={(e)=>{setCurrentLink({...currentLink, active: String(e.target.checked)})}}
-              /* {...formik.getFieldProps('email')} */
-            />
-            </p>
-            <input 
-              type="text"
-              name='index'
-              placeholder='index'
-              value={currentLink.index}
-              className={styles.input_text}
-              onChange={(e)=>setCurrentLink({...currentLink, index : e.target.value})}
-              /* {...formik.getFieldProps('email')} */
-            />
-            <div className="input-button">
-              <button type='submit' className="w-1/3 bg-gradient-to-r from-rose-500 to-fuchsia-500 rounded-md py-3 text-gray-50 text-lg">
-                Save
-              </button>
-              <button 
-                className="w-1/3 bg-zinc-100 rounded-md py-3 text-gray-800 text-lg"
-                onClick={(e) => handleCancelEditLink(e)}>
-                Cancel
-              </button>
-            </div>
-          </form>
-        </div>
-      </section>
-    )
+  
+  function handleFormSubmit(e){
+    e.preventDefault()
+    console.log("form submitted")
   }
-  else{
-      return (
-          <section className="container mx-auto text-center py-20 w-4/5 sm:w-1/2">
-              <div className='flex flex-col items-center'>
-                  {showAddLink 
-                      ?
-                      <form>
-                          <input 
-                              type="text"
-                              name='title'
-                              placeholder='title'
-                              className={styles.input_text}
-                              /* {...formik.getFieldProps('email')} */
-                              />
-                          <input 
-                              type="text"
-                              name='url'
-                              placeholder='url'
-                              className={styles.input_text}
-                              /* {...formik.getFieldProps('email')} */
-                              />
-                          <input 
-                              type="checkbox"
-                              name='active'
-                              placeholder='active'
-                              className={styles.input_text}
-                              /* {...formik.getFieldProps('email')} */
-                              />
-                          <div className="input-button">
-                              <button type='submit' className="w-1/3 bg-gradient-to-r from-rose-500 to-fuchsia-500 rounded-md py-3 text-gray-50 text-lg">
-                                  Save
-                              </button>
-                              <button 
-                                  className="w-1/3 bg-zinc-100 rounded-md py-3 text-gray-800 text-lg"
-                                  onClick={(e) => handleCancelAddLink(e)}>
-                                  Cancel
-                              </button>
-                          </div>
-                      </form>
-                      : <div className='flex flex-col items-center justify-center align-center border-2 w-10 h-10 cursor-pointer rounded-full bg-zinc-50' onClick={(e)=> handleAddLink(e)}>
-                          <HiOutlinePlusSm />
+
+  
+  return (
+      <section className="container mx-auto text-center py-20 w-4/5 sm:w-1/2">
+          <div className='flex flex-col'>
+              {showAddLink || showEditLink
+                  ?
+                  <form onSubmit={(e)=>handleFormSubmit(e)}>
+                    <div className='grid grid-cols-4 gap-4'>
+                      <div className='col-span-4'>
+                        <div className='flex flex-col w-fu  ll'>
+                          <div className='flex self-center justify-center items-center w-10 h-10 cursor-pointer rounded-full' onClick={(e) => handleCancel(e)}>
+                            <HiOutlineXCircle size={25}/></div>
+                        </div>
                       </div>
-                  }
-                  {links.map(link =>(
-                      <div key = {link._id} className='flex flex-row justify-between w-full bg-zinc-50 hover:bg-zinc-100 py-3 m-3 rounded-full 
-                          cursor-pointer border-2 border-zinc-100 hover:border-zinc-200 hover:border-dashed text-rose-950'> 
-                          <p className="px-4">title: {link.title} | url:{link.url} | active:{link.active} | index: {link.index}</p> 
-                          <span className='icon flex items-center px-4' onClick={(e) => handleEditLink(e, link)}><HiOutlinePencil /></span>
+                      <div className='col-span-4'>
+                        <input 
+                          type="text"
+                          name='title'
+                          placeholder='title'
+                          value={currentLink.title}
+                          className={styles.input_text}
+                          onChange={(e)=>setCurrentLink({...currentLink, title : e.target.value})}
+                          /* {...formik.getFieldProps('email')} */
+                        />
                       </div>
-                  ))}    
-              </div>  
-              <main className="container mx-auto text-center py-20">
-                  <h4 className='text-4xl font-bold'>Authorized User</h4>
-      
-                  <div className='details'>
-                      <h5>{session.user.name}</h5>
-                      <h5>{session.user.email}</h5>
+                      <div className='col-span-4'>
+                        <input 
+                          type="text"
+                          name='url'
+                          placeholder='url'
+                          value={currentLink.url}
+                          className={styles.input_text}
+                          onChange={(e)=>setCurrentLink({...currentLink, url : e.target.value})}
+                          /* {...formik.getFieldProps('email')} */
+                        />
+                      </div>
+                      {showEditLink 
+                        ?
+                        <div className='col-span-4'>
+                          <input 
+                            type="text"
+                            name='index'
+                            placeholder='index'
+                            value={currentLink.index}
+                            className={styles.input_text}
+                            onChange={(e)=>setCurrentLink({...currentLink, index : e.target.value})}
+                            /* {...formik.getFieldProps('email')} */
+                          />
+                        </div>
+                        :
+                        <></>
+                      }
+                      <div><span>active:</span></div>
+                      <div className='col-span-3'>
+                        <input 
+                          type="checkbox"
+                          name='active'
+                          placeholder='active'
+                          className={styles.input_text}
+                          checked={currentLink.active === 'true'}
+                          onChange={(e)=>{setCurrentLink({...currentLink, active: String(e.target.checked)})}}
+                          /* {...formik.getFieldProps('email')} */
+                        />
+                      </div>
+                      <div className='col-span-4'>
+                        <div className='flex flex-col w-full'>
+                          <button type= 'submit' className='flex self-center justify-center items-center w-10 h-10 cursor-pointer rounded-full'>
+                            <HiOutlineCloudUpload size={25}/></button>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                  :
+                  <div className='flex flex-col w-full'>
+                    <div className='flex self-center justify-center items-center w-10 h-10 cursor-pointer rounded-full' onClick={(e)=> handleAddLink(e)}>
+                      <HiOutlinePlusSm  size={25}/>
+                    </div>
                   </div>
-      
-                  <div className="flex justify-center">
-                      <button onClick={handleSignOut} className='mt-5 px-10 py-1 rounded-sm bg-indigo-500'>Sign Out</button>
+              }
+              {links.map(link =>(
+                  <div key = {link._id} className='flex flex-row justify-between w-full bg-zinc-50 hover:bg-zinc-100 py-3 m-3 rounded-full 
+                      cursor-pointer border-2 border-zinc-100 hover:border-zinc-200 hover:border-dashed text-rose-950'> 
+                      <p className="px-4">title: {link.title} | url:{link.url} | active:{link.active} | index: {link.index}</p> 
+                      <span className='icon flex items-center px-4' onClick={(e) => handleEditLink(e, link)}><HiOutlinePencilAlt size={25} /></span>
                   </div>
-              </main>    
-          </section>
-      )
-    }   
+              ))}    
+          </div>  
+          <main className="container mx-auto text-center py-20">
+              <h4 className='text-4xl font-bold'>Authorized User</h4>
+  
+              <div className='details'>
+                  <h5>{session.user.name}</h5>
+                  <h5>{session.user.email}</h5>
+              </div>
+  
+              <div className="flex justify-center">
+                  <button onClick={handleSignOut} className='mt-5 px-10 py-1 rounded-sm bg-indigo-500'>Sign Out</button>
+              </div>
+          </main>    
+      </section>
+  )
+    
 }
 
 
