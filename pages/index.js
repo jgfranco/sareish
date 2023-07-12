710494409ce141aaf53b7deecdee7742698caef9
Juan Gerardo Franco <juangerardofranco@gmail.com>
2023-07-12 10:58:51 -0500

    fixed page height in linksEditor

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ function Guest({links}){
   });
 
   return (
-      <section className="container mx-auto text-center py-10 w-4/5 sm:w-1/2 h-screen">
+      <section className="container mx-auto text-center py-10 w-4/5 sm:w-1/2 h-fit">
         <div className="mb-8"> 
           <Image className='rounded-full' src='/assets/profileSareish.jpeg' width={120} height={120}></Image>
         </div>
