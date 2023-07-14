4202ca27ff7b422d945b27a1336d98e9a2903dba
Juan Gerardo Franco <juangerardofranco@gmail.com>
2023-07-14 15:21:43 -0500

    rearrange of links data in linkseditor

--- pages/index.js
+++ pages/index.js
@@ -45,14 +45,13 @@ function Guest({links}){
     const newWindow = window.open(link.url, '_blank', 'noopener,noreferrer');
     if (newWindow) newWindow.opener = null
     e.preventDefault();
-    //setCurrentLink(link)
+
     formik.values._id = link._id
     formik.values.title = link.title;
     formik.values.url = link.url;
     link.active === 'true' ? formik.values.active = true : formik.values.active = false;
     formik.values.index = link.index;
     onSubmit(formik.values);
-
   }
 
   async function onSubmit(values){
@@ -76,8 +75,6 @@ function Guest({links}){
         {/* <h1>under construction</h1> */}
         <div className='flex flex-col justify-center '>  
           {links.map(link =>(
-        
-            
             <div className='w-full bg-rose-50 hover:bg-rose-100 py-3 my-3 rounded-full 
               cursor-pointer border-2 border-rose-100 hover:border-rose-200 hover:border-dashed text-rose-950'
               onClick={(e) =>onLinkClick(e, link)}
