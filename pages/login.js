a5bde2159f8af96de97516bdf1dc29057e4d6780
Juan Gerardo Franco <juangerardofranco@gmail.com>
2023-07-21 23:32:00 -0500

    styling of login and register page

--- pages/login.js
+++ pages/login.js
@@ -52,7 +52,7 @@ export default function Login(){
             <Head>
                 <title>Login</title>
             </Head>
-            <section className='w-3/4 mx-auto flex flex-col gap-10'>
+            <section className='flex flex-col w-3/4 mx-auto gap-10'>
                 <div className="title">
                     <h1 className='text-zinc-800 text-4xl font-bold py-4'>Login</h1>
                     {/* <p className='w-3/4 mx-auto text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, officia?</p> */}
