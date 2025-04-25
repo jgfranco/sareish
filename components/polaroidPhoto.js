2eedb7db6e67fd9e51d3d69f5145868ab09051dc
Gerardo Franco <juangerardofranco@gmail.com>
2025-04-24 19:09:23 -0500

    css formatting and renaming of main function

--- components/polaroidPhoto.js
+++ components/polaroidPhoto.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function PolaroidPhoto({ src, alt, caption }) {
   return (
-    <div className="bg-white shadow-lg border border-gray-150 rounded-sm p-4 w-fit text-center transform transition-transform hover:scale-105">
+    <div className="bg-white shadow-lg border border-gray-150 rounded-sm p-4 w-full text-center transform transition-transform hover:scale-105">
       <div className="bg-white pb-4 rounded-sm overflow-hidden">
-        <div className="relative w-60 h-72 mx-auto">
+        <div className="relative w-30 h-40 mx-auto">
           <Image
             src={src}
             alt={alt}
