cc7f12b5a0f7c9c419e5ec85451fdc6c0254cf8a
Gerardo Franco <juangerardofranco@gmail.com>
2025-05-12 18:55:20 -0500

    photoView links are now responsive, they open the link in another tab

--- components/photoView.js
+++ components/photoView.js
@@ -2,15 +2,15 @@ import PolaroidPhoto from "./polaroidPhoto";
 
 export default function PhotoView({ links }) {
     return (
-        <div className="grid grid-cols-2 w-full gap-3">  
+        <div className="grid grid-cols-2 w-full gap-3" >  
         {links.map(link =>(
-            <PolaroidPhoto 
-            key={link.index}
+            <PolaroidPhoto     
+            key = {link.index}
             src = {link.photoUrl}
             alt = {link.title}
-            caption = {link.title}/>
+            caption = {link.title}
+            url={link.url}/>
         ))}
         </div>
     );
-  }
-  
+}
